246a0eb8132eaf9c425c845935bd3ed5b26dc20b
jmls9627 <jmls9627@gmail.com>
2020-04-10 12:29:36 -0400

    Retrieving Data Using HTTP

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -14,7 +14,8 @@ export class ProductListComponent implements OnInit {
     imageWidth:number=50;
     imageMargin:number=2;
     showImage:boolean=false;
-    
+    errorMessage:string;
+
     _listFilter:string;
     get listFilter():string{
         return this._listFilter;
@@ -48,8 +49,13 @@ export class ProductListComponent implements OnInit {
     }
 
      ngOnInit():void{
-      this.products=this.productService.getProducts() ;
-      this.filteredProducts=this.products;
+     this.productService.getProducts().subscribe({
+       next: products=>{
+         this.products=products
+         this.filteredProducts=this.products;
+      },
+       error: err => this.errorMessage=err
+     }) ;
       
      }
 }
