0e06044f353e46c9fb1c5c32ff1ea7dd32eba793
jmls9627 <jmls9627@gmail.com>
2020-04-13 11:09:00 -0400

    Navigation and Routing Additional Techniques

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 
 
@@ -27,11 +28,15 @@ import { WelcomeComponent } from './home/welcome.component';
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-  {path: 'products',component:ProductListComponent},
-  {path: 'products/:id',component:ProductDetailComponent},
-  {path: 'welcome',component:WelcomeComponent},
-  {path: '', redirectTo: 'welcome', pathMatch:'full'},
-  {path: '**', redirectTo: 'welcome', pathMatch:'full'}
+          {path: 'products',component:ProductListComponent},
+          {
+            path: 'products/:id', 
+          canActivate:[ProductDetailGuard],
+          component:ProductDetailComponent
+          },
+          {path: 'welcome',component:WelcomeComponent},
+          {path: '', redirectTo: 'welcome', pathMatch:'full'},
+          {path: '**', redirectTo: 'welcome', pathMatch:'full'}
     ])
   ],
   bootstrap: [AppComponent]
--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,18 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './products';
 
+import { ActivatedRoute, Router } from '@angular/router';
+
 @Component({
-  selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-pagetitle:string='Product Detail';
+pageTitle:string='Product Detail';
 product:IProduct;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute, private router:Router) { }
 
   ngOnInit() {
+    let id= +this.route.snapshot.paramMap.get('id');
+    this.pageTitle +=`: ${id}`;
+    this.product= {
+        "productId": id,
+        "productName": "Leaf Rake",
+        "productCode": "GDN-0011",
+        "releaseDate": "March 19, 2019",
+        "description": "Leaf rake with 48-inch wooden handle.",
+        "price": 19.95,
+        "starRating": 3.2,
+        "imageUrl": "assets/images/leaf_rake.png"
+      }
+    
   }
-
+     onback():void{
+    this.router.navigate(['/products']);
+ }
 }
